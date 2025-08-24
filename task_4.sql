3807fccf6c4b01db043948aad8226269662d54c4
MSEC <msec@localhost.localdomain>
2025-08-24 22:41:23 +0000

    printing full description of the table from alx-book-store

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,8 @@
 USE alx_book_store
--- Script to print the full description of the table books
-SHOW CREATE TABLE books;
+-- Script to print the full description of the table books from alx_book_store
+
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books'
+ORDER BY ORDINAL_POSITION;
